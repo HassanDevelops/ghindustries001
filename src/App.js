2dc2c3aa348969d4c3b2774d0a3a74a2fccb6b7e
HassanDevelops <59549481+HassanDevelops@users.noreply.github.com>
2020-01-11 20:59:31 +0500

    added Layout to Products and About Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from "react";
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
 import Layout from "./layouts/Layout";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <CssBaseline />
-      <Layout>
-        <Home />
-      </Layout>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Layout>
+              <Home />
+            </Layout>
+          </Route>
+          <Route path="/about" exact>
+            <Layout>
+              <About />
+            </Layout>
+          </Route>
+          <Route path="/products" exact>
+            <Layout>
+              <Products />
+            </Layout>
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
